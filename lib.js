20679d65e9800ae0a162f5eafa55041b96919f03
Sam Pepler <sam.pepler@stfc.ac.uk>
2018-10-03 15:47:27 +0100

    different stream view with variable length badge time line.

--- lib.js
+++ lib.js
@@ -100,33 +100,84 @@ function icons(log) {
     return w
 }
 
+
+// make a utc date time from a string
+function utc_from_str(s) {
+    // replace the T
+    s = s.substr(0, 10) + ' ' + s.substr(11, 19) + " GMT";
+    return new Date(s);
+}
+
+// make now
+function now() {
+    var now = new Date;
+    var utc_timestamp = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),
+        now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds(), now.getUTCMilliseconds()));
+    return utc_timestamp;
+}
+
 // period job display
-function periodbadge(log, do_millisec) {
-    var start_time = new Date(log.start_time.substring(0, 19));
-    var end_time = new Date(log.end_time.substring(0, 19));
-    var ms = log.end_time.substring(17) - log.start_time.substring(17);
-
-
+function log_periodbadge(log) {
     if (log.state == "running") {
-        end_time = new Date()
+        return periodbadge(log.start_time);
+    } else {
+        return periodbadge(log.start_time, log.end_time);
     }
+}
+
+// period job display
+function periodbadge(start_time, end_time) {
+    start_time = utc_from_str(start_time);
+    if (end_time === undefined) {
+        end_time = new Date();
+    } else if (end_time.substring(0, 4) == "1970") {
+        end_time = start_time;
+    } else {
+        end_time = utc_from_str(end_time);
+    }
+    //var ms = log.end_time.substring(17) - log.start_time.substring(17);
+
     var p = end_time - start_time;
-    if (p < 2000) {
-        if (do_millisec) {return '<span class="badge badge-info">'+ms.toFixed(5)+" s</span>"}
-        else {return ''}
-    }
+    p = Math.max(p, 0);
+    console.log(end_time, start_time, p);
+
+    var logp = Math.round(Math.pow((p/1000+0.01), 0.3)*3);
+    if (logp <= 1) {logp=1}
+    var svg = '<svg width="'+logp+'" height="5"></svg>';
     p = p/1000;
-    if (p < 120) {return '<span class="badge badge-info">'+Math.round(p)+" s</span>"}
+    if (p < 20) {return '<span class="badge badge-secondary">'+svg+' '+Math.round(p)+" s "+svg+"</span>"}
+    if (p < 120) {return '<span class="badge badge-primary">'+svg+' '+Math.round(p)+" s "+svg+"</span>"}
     p = p/60;
-    if (p < 90) {return '<span class="badge badge-info">'+Math.round(p)+" min</span>"}
+    if (p < 90) {return '<span class="badge badge-info">'+svg+' '+Math.round(p)+" min "+svg+"</span>"}
     p = p/60;
-    if (p < 50) {return '<span class="badge badge-warning">'+Math.round(p)+" hrs</span>"}
+    if (p < 50) {return '<span class="badge badge-warning">'+svg+' '+Math.round(p)+" hrs "+svg+"</span>"}
     p = p/24;
-    return '<span class="badge badge-danger">'+Math.round(p)+" days</span>"
+    return '<span class="badge badge-danger" style="float: left">'+svg+' '+Math.round(p)+" days "+svg+"</span>"
 }
 
 // spacer
 function spacer(p, last_p) {
+    var c = '';
+    if (p < last_p*1.01 && p > last_p*0.99) {c = "white"} else {c = "#220"}
+
+    var s = '<svg height="15" width="50"> <path d="M0 7 L7 0 L45 0 L45 15 L7 15 L0 7" fill="'+c+'" stroke="black"/>';
+    s += '<text x=7 y=11 fill="gray" style="font-family: monospace, Courier New; font-weight: bold; font-style: normal; font-size: 10px">';
+    var unit;
+
+    if (p < 2000) {return ''}
+    p = p/1000; unit = "s";   // miliseconds to seconds
+    if (p < 120) {return ''}
+    p = p/60; unit = "min";     // seconds to minutes
+    if (p < 90) {return s + Math.round(p) + " " + unit + "</text></svg>"}
+    p = p/60; unit = "hrs";     // mins to hours
+    if (p < 50) {return s + Math.round(p) + " " + unit + "</text></svg>"}
+    p = p/24; unit = "days";     // hours to days
+    return s + Math.round(p) + " " + unit + "</text></svg>";
+
+}
+
+// spacer
+function spacer2(p, last_p) {
     var c = '';
     if (p < last_p*1.01 && p > last_p*0.99) {c = "white"} else {c = "#220"}
 
@@ -188,7 +239,7 @@ function stream_button(log) {
 }
 
 
-function job_button(log) {
+function job_button(log, last_log) {
     var state = log.state;
     var job_name = log.jobname;
     var start_time = new Date(log.start_time.substring(0, 19));
@@ -199,9 +250,9 @@ function job_button(log) {
     var ago = Math.floor((new Date() - end_time) / (1000 * 60));
     var recent = (ago < 5);
 
-    var b = periodbadge(log);
+    var b = log_periodbadge(log);
 
-    var boot_class = "mb-1 btn btn-sm";
+    var boot_class = "mb-1 btn";
     boot_class += " btn-outline-" + colours[state];
     if (recent || state == "running") {
         boot_class += " active"
@@ -212,10 +263,17 @@ function job_button(log) {
     var p = make_params();
     p["job"] = job_name;
     var url = 'job.html?' + $.param(p);
-    var w = '<a href="'+url+'" class="' + boot_class + '">' + label + ' ' +b;
 
-    w += icons(log);
-    w += '</a> ';
+    var job_start = new Date(log.start_time.substring(0, 19));
+    var last_job_start = new Date(last_log.start_time.substring(0, 19));
+    var space = last_job_start - job_start;
+    //var s = spacer(space, 0);
+    var s = '<div style="float: right">' + periodbadge(log.start_time, last_log.start_time) + '</div>';
+
+    var w = '<div style="float: left" class="mb-3 mr-1">';
+    w += '<div style="float: right"><a href="'+url+'" class="' + boot_class + '"><small>' + label + ' ' + icons(log) + '</small></a></div>';
+    w += '<br/><div style="float: right" class="mb-1">' + b + '</div>';
+    w += '<br/><div style="float: right">'+s+'</div>';
     return w;
 }
 
@@ -238,3 +296,5 @@ function button_filter(f) {
     var checkbox = $('#'+ f.id+' :input');
     checkbox.prop("checked", !checkbox.prop("checked"));
 }
+
+//C7B9X6W4L6
